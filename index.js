18647c5bf0992850aeff074124b6cb6fd567908a
fzhao888 <fzhao6@depaul.edu>
2022-05-04 19:20:45 -0500

    d

--- index.js
+++ index.js
@@ -37,24 +37,17 @@ if (process.env.DATABASE_URL != null){
 
 else{
    connectionParams = {
-	host: 'willowrealestate.postgres.database.azure.com',
-    user: 'team5',
-    password: 'Willow5!',
-    database: 'postgres',
-    port: 5432,
-    ssl: true
+	user: 'team3_user',
+   	host: 'localhost',
+  	database: 'team3',
+  	password: 'team3pass',
+  	port: 5432
   }
 }
 
 console.log(connectionParams)
 const pool = new pg.Client(connectionParams)
 
-pool.connect(err => {
-    if (err) throw err;
-    else {
-        console.log("connection error")
-    }
-});
  
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Willow' });
